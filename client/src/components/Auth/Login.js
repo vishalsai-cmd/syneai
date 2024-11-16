9191cfd6e93c315309c3bca569bd5a7b7b75942e
vishalsai-cmd <129299772+vishalsai-cmd@users.noreply.github.com>
2024-11-16 12:07:19 +0530

    Update Login.js

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('/api/auth/login', { email, password });
+      const response = await axios.post('syneai.vercel.app/api/auth/login', { email, password });
       localStorage.setItem('token', response.data.token);
       navigate('/cars');
     } catch (error) {
