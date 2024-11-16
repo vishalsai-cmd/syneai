53721b1084271a794a02d4a307adfe136c49cfc9
vishal <vishalsair2005@gmail.com>
2024-11-16 11:43:56 +0530

    first

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
